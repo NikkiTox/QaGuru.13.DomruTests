f906204ffd63af64d042b2ef4bb4260f6e74d7dd
Nikita Miroshnichenko <Niki.tone@yandex.ru>
2022-04-28 23:43:10 +0500

    Initial Commit

---
+++ src/test/java/ru/dom/ufa/config/Project.java
@@ -0,0 +1,23 @@
+package ru.dom.ufa.config;
+import org.aeonbits.owner.ConfigFactory;
+
+public class Project {
+    public static WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+
+    public static boolean isRemoteWebDriver() {
+        return !webConfig.remoteUrl().equals("");
+    }
+
+    public static String browserNameChose() {
+        return webConfig.browserNameChose();
+    }
+
+    public static String browserVersion() {
+        return webConfig.browserVersion();
+    }
+
+    public static String browserSize() {
+        return webConfig.browserSize();
+    }
+
+}
---
+++ src/test/java/ru/dom/ufa/config/WebConfig.java
@@ -0,0 +1,19 @@
+package ru.dom.ufa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:${launch}.properties")
+public interface WebConfig extends Config {
+
+    @Key("browserNameChose")
+    String browserNameChose();
+
+    @Key("browserVersion")
+    String browserVersion();
+
+    @Key("browserSize")
+    String browserSize();
+
+    @Key("remoteUrl")
+    String remoteUrl();
+}
